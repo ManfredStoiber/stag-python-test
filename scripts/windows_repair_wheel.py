a48cb983d4d9bc397eff4a30e04dd57bf058e137
Manfred Stoiber <Manfred.Stoiber@gmail.com>
2023-11-30 10:13:43 +1300

    try to improve packaging

---
+++ scripts/windows_repair_wheel.py
@@ -0,0 +1,23 @@
+import os
+import re
+import subprocess
+import sys
+
+pattern = re.compile(".*temp.*Release.*opencv.*bin.*Release")
+
+# find opencv dll directory
+for d in os.walk('.'):
+    if pattern.fullmatch(d[0]):
+        path = d[0]
+        break
+
+# run delvewheel
+print("Run command: delvewheel repair (without arguments)", flush=True)
+subprocess.run(["delvewheel",  "repair"])
+
+print("Run command: ", flush=True)
+print(" ".join(["delvewheel",  "repair", "--add-path", f"{path}", "-w", f"{sys.argv[1]}", f"{sys.argv[2]}"]))
+print(f"Check if {path} exits: {os.path.exists(path)}")
+print(f"Check if {sys.argv[1]} exits: {os.path.exists(sys.argv[1])}", flush=True)
+print(f"Check if {sys.argv[2]} exits: {os.path.exists(sys.argv[2])}", flush=True)
+subprocess.run(["delvewheel",  "repair", "--add-path", f"{path}", "-w", f"{sys.argv[1]}", f"{sys.argv[2]}"])
